412b91172499dd513a3a00431cc348ca1f74cc2c
Atharva Dumbre <84585013+31Atharva@users.noreply.github.com>
2025-04-13 18:15:59 +0000

    Basics

---
+++ basics/nums_and_maths.js
@@ -0,0 +1,9 @@
+const score = 400
+console.log(score)
+
+const balance = new Number (1000)
+console .log(balance)
+
+console.log(balance.toString())
+console.log(balance.toString().length)
+console.log(balance.toFixed(2))
