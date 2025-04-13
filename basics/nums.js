873e0944a3e071b004b98e2dece2ae57c1fe3900
Atharva Dumbre <84585013+31Atharva@users.noreply.github.com>
2025-04-13 18:43:58 +0000

    Basics

---
+++ basics/nums.js
@@ -0,0 +1,20 @@
+const score = 400
+console.log(score)
+
+const balance = new Number (1000)
+console .log(balance)
+
+console.log(balance.toString())
+console.log(balance.toString().length)
+console.log(balance.toFixed(2))
+
+const otherNumber = 23.456
+console.log(otherNumber.toPrecision(3))
+
+const hundresds = 100000000
+console.log(hundresds.toLocaleString('en-IN'))
+console.log(hundresds.toLocaleString('en-US'))
+
+console.log(Number.MAX_VALUE)
+console.log(Number.MIN_VALUE)
+console.log(Number.MAX_SAFE_INTEGER)
