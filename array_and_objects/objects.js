751c7238ef9337e0d7bd68d68ddff1b84e716777
Atharva Dumbre <84585013+31Atharva@users.noreply.github.com>
2025-04-15 17:24:44 +0000

    Objects

--- array_and_objects/objects.js
+++ array_and_objects/objects.js
@@ -2,9 +2,11 @@
 
 // object literal
 
+const mySym = Symbol("Key1")
 const JsUser = {
     name: "Atharva",
     age: 20,
+    [mySym]: "Key1", //declare symbol
     isStudent: true,
     hobbies: ["coding", "reading"],
     address: {
@@ -18,3 +20,21 @@ const JsUser = {
 
 console.log(JsUser.name) // access object property
 console.log(JsUser["name"]) // access object property
+console.log(JsUser[mySym]) // access symbol property
+console.log(typeof([mySym])) // check type of symbol
+
+JsUser.name = "Atharva" // update object property
+console.log(JsUser.name) // access object property
+
+JsUser.greeting = function(){
+    console.log("Hello JS User");
+}
+
+JsUser.greetingTwo = function(){
+    console.log(`Hello JS User, ${this.name}`);
+}
+
+Object.freeze(JsUser); // freeze object after adding methods
+
+console.log(JsUser.greeting()); // access object property
+console.log(JsUser.greetingTwo()); // access object property
