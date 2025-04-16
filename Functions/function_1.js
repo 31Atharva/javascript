1e1fe046e521ae1ddea7badb054f680c65602f57
Atharva Dumbre <84585013+31Atharva@users.noreply.github.com>
2025-04-16 14:51:40 +0000

    Functions

--- Functions/function_1.js
+++ Functions/function_1.js
@@ -8,30 +8,30 @@ function sayMyName(name){
     console.log('A')
 }
 
-//sayMyName()
+sayMyName()
 
-// function add(a, b){
-//     console.log(a + b);
-// }
+function add(a, b){
+    console.log(a + b);
+}
 
-// add(2, 3)
-// add(5," 10")
-// add(5,'a')
+add(2, 3)
+add(5," 10")
+add(5,'a')
 
-// function add(a, b){
-//     let result = a + b;
-//     return result;
-// }
-
-// const result = add(2,4)
-// console.log(result)
-
-// function add(a, b){
-//     return (a+b)
-// }
+function add(a, b){
+    let result = a + b;
+    return result;
+}
 
 //const result = add(2,4)
-//console.log(result)
+console.log(result)
+
+function add(a, b){
+    return (a+b)
+}
+
+const result = add(2,4)
+console.log(result)
 
 function loginUserMessage(userName){
     if(userName === undefined){
