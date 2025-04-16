37f025a70a8433156f4421bf6482a632a903c3e7
Atharva Dumbre <84585013+31Atharva@users.noreply.github.com>
2025-04-16 16:11:47 +0000

    Scope

---
+++ Functions/scope_1.js
@@ -0,0 +1,33 @@
+// dont use var
+// use let and const
+// let and const are block scoped
+// var is function scoped
+let a = 10
+const b = 20
+var c = 30
+d = 40
+
+if (true){
+    let a = 100
+    const b = 200
+    var c = 300
+    d = 400
+}
+
+console.log(a)
+console.log(b)
+console.log(c)
+console.log(d)
+
+// let and const are block scoped
+
+let x = 10
+const y = 20
+
+if (true){
+    let x = 100
+    const y = 200
+    console.log('Inside if block',x)
+}
+console.log(x)
+console.log(y)
