45b7ce45b0d18ae9615704789d78875476bf16ab
Atharva Dumbre <84585013+31Atharva@users.noreply.github.com>
2025-04-22 17:44:19 +0000

    forEach

--- iterations/five.js
+++ iterations/five.js
@@ -1,1 +1,16 @@
- 
+ const coding = ['JavaScript', 'Python', 'Java', 'C++', 'Ruby'];
+
+ coding.forEach(function(item){
+    //console.log(item);
+    //console.log(index
+ })
+
+ coding.forEach((item) => {
+    //console.log(item);
+    //console.log(index
+ })
+
+ function printMe(item){
+    console.log(item);
+ }
+coding.forEach(printMe);
