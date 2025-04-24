4ebe8ddbfe38435dabdf1816096f7eb864830502
Atharva Dumbre <84585013+31Atharva@users.noreply.github.com>
2025-04-24 17:38:17 +0000

    forEach

--- iterations/five.js
+++ iterations/five.js
@@ -14,3 +14,27 @@
     console.log(item);
  }
 coding.forEach(printMe);
+
+// coding.forEach( (item, index, arr)=> {
+//     console.log(item, index, arr);
+// } )
+
+const myCoding = [
+   {
+       languageName: "javascript",
+       languageFileName: "js"
+   },
+   {
+       languageName: "java",
+       languageFileName: "java"
+   },
+   {
+       languageName: "python",
+       languageFileName: "py"
+   },
+]
+
+myCoding.forEach( (item) => {
+   
+   console.log(item.languageName);
+} )
